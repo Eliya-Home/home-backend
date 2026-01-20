bf54e6e0a6d8b2694148892a778f59fc71ab32ed
Eliya-Home <eliyaisaya97@gmail.com>
2026-01-20 18:41:31 +0300

    Add files via upload

---
+++ adminRoutes.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const Agent = require("../models/Agent");
+
+const router = express.Router();
+
+/* GET ALL AGENTS (FOR ADMIN) */
+router.get("/agents", async (req, res) => {
+    try {
+        const agents = await Agent.find();
+        res.json(agents);
+    } catch (err) {
+        res.status(500).json({ message: "Error kupata agents" });
+    }
+});
+
+/* VERIFY AGENT */
+router.put("/verify/:id", async (req, res) => {
+    try {
+        await Agent.findByIdAndUpdate(req.params.id, { verified: true });
+        res.json({ message: "Agent ameverified" });
+    } catch (err) {
+        res.status(500).json({ message: "Verification imeshindikana" });
+    }
+});
+
+module.exports = router;
